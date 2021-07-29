fbad2ac6afc485b2d6e4b8327d8634be2f4969d3
Argonyte <54732393+Argonyte@users.noreply.github.com>
2021-07-29 17:50:27 +0200

    Go Learning Projects

---
+++ GoLang Projects/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go"+"lang") //string concat with +
+	
+	fmt.Println(" 1+1 = ", 1+1) //integers
+	fmt.Println(" 7.0/3.0 = ", 7.0/3.0) //floats
+
+	fmt.Println( true && false ) //boolean Operations
+	fmt.Println( true || false )
+	fmt.Println( !true )
+}
